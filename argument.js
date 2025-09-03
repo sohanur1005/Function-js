3526d4fbbb0af52a3bbcec8d27d3cc1e2085dae1
sohanur24059 <sohanurrahman24059@gmail.com>
2025-09-03 12:04:48 +0600

    send this

---
+++ argument.js
@@ -0,0 +1,18 @@
+function double(num){
+    const doubled= num*2;
+    console.log(doubled)
+}
+double(10);
+const  money=20;
+double(55)
+const number=50;
+double(50)
+
+// Difference
+function difference(num1,num2){
+    const diff=num1-num2;
+    console.log(num1,num2,'Difference is :',diff)
+}
+const n1=20;
+const  n2=10;
+difference(n1,n2)

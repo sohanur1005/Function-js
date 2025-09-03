f227e75a6231a4367050050b758e40d8a8b185f9
sohanur24059 <sohanurrahman24059@gmail.com>
2025-09-03 21:46:16 +0600

    send this

---
+++ conditional.js
@@ -0,0 +1,13 @@
+function isEven(num){
+    if(num%2==0)
+    {
+        return true;
+    }
+    else{
+       return false;
+    }
+}
+const test1=isEven(6);
+const test2=isEven(5);
+console.log(test1)
+console.log(test2)
---
+++ return.js
@@ -0,0 +1,13 @@
+function tenTime(num){
+    const result=num*10;
+    return result;
+}
+const output=tenTime(50);
+console.log(output)
+
+function add  (n1,n2){
+    
+    return n1+n2;
+}
+const bill=add(10,20);
+console.log(bill)

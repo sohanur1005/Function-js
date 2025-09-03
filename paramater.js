da0ee76e8c76bb994de9d01c65eaf7e872d3a8f7
sohanur24059 <sohanurrahman24059@gmail.com>
2025-09-03 10:46:45 +0600

    send this

---
+++ paramater.js
@@ -0,0 +1,15 @@
+function square(num){
+    const borgo=num * num;
+    console.log('Square is :',borgo)
+
+}
+square(5);
+square(7);
+square(10);
+
+// Add Number
+function add(num1,num2){
+    const sum=num1+ num2;
+    console.log('Sum :',sum)
+}
+add(5,5);
